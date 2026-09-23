3c136795be9d2f4329100151388a75d3d19fb8bc
Mehmet Kaya <mrtkaya2734@gmail.com>
2026-09-23 17:45:48 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,1 +1,38 @@
 
+package com.hurdatakip.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        // Render üzerinde çalışan canlı sitenin adresini buraya yaz
+        webView.loadUrl("https://hurda-fiyat-bot.onrender.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
